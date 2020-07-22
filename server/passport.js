62b816c27037104e16dd0e4239c5fd8e837f07d7
htrix <htrixe@gmail.com>
2020-07-22 09:13:31 +0100

    facebook Oauth implemented

--- server/passport.js
+++ server/passport.js
@@ -2,8 +2,8 @@ const passport = require("passport");
 const JwtStrategy = require("passport-jwt").Strategy;
 const { ExtractJwt } = require("passport-jwt");
 const LocalStrategy = require("passport-local").Strategy;
-const GooglePlusTokenStrategy = require('passport-google-plus-token');
-const config = require('./configuration');
+const GooglePlusTokenStrategy = require("passport-google-plus-token");
+const config = require("./configuration");
 const User = require("./models/user");
 
 // JSON WEB TOKENS STRATEGY
@@ -64,32 +64,74 @@ passport.use(
   )
 );
 // Google OAuth Strategy
-passport.use('googleToken', new GooglePlusTokenStrategy({
-  clientID: config.oauth.google.clientID,
-  clientSecret: config.oauth.google.clientSecret
-}, async (accessToken, refreshToken, profile, done) => {
-  try {
-    // Should have full user profile over here
-    console.log('profile', profile);
-    console.log('accessToken', accessToken);
-    console.log('refreshToken', refreshToken);
+passport.use(
+  "googleToken",
+  new GooglePlusTokenStrategy(
+    {
+      clientID: config.oauth.google.clientID,
+      clientSecret: config.oauth.google.clientSecret,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        // Should have full user profile over here
+        console.log("profile", profile);
+        console.log("accessToken", accessToken);
+        console.log("refreshToken", refreshToken);
 
-    const existingUser = await User.findOne({ "google.id": profile.id });
-    if (existingUser) {
-      return done(null, existingUser);
-    }
+        const existingUser = await User.findOne({ "google.id": profile.id });
+        if (existingUser) {
+          return done(null, existingUser);
+        }
 
-    const newUser = new User({
-      method: 'google',
-      google: {
-        id: profile.id,
-        email: profile.emails[0].value
+        const newUser = new User({
+          method: "google",
+          google: {
+            id: profile.id,
+            email: profile.emails[0].value,
+          },
+        });
+
+        await newUser.save();
+        done(null, newUser);
+      } catch (error) {
+        done(error, false, error.message);
       }
-    });
+    }
+  )
+);
 
-    await newUser.save();
-    done(null, newUser);
-  } catch (error) {
-    done(error, false, error.message);
-  }
-}));
+// Facebook OAuth Strategy
+passport.use(
+  "facebookToken",
+  new FacebookTokenStrategy(
+    {
+      clientID: config.oauth.facebook.clientID,
+      clientSecret: config.oauth.facebook.clientSecret,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        console.log("profile", profile);
+        console.log("accessToken", accessToken);
+        console.log("refreshToken", refreshToken);
+
+        const existingUser = await User.findOne({ "facebook.id": profile.id });
+        if (existingUser) {
+          return done(null, existingUser);
+        }
+
+        const newUser = new User({
+          method: "facebook",
+          facebook: {
+            id: profile.id,
+            email: profile.emails[0].value,
+          },
+        });
+
+        await newUser.save();
+        done(null, newUser);
+      } catch (error) {
+        done(error, false, error.message);
+      }
+    }
+  )
+);
